935bf3b8020555c3c1d2a16ac356f2bdee72974f
douglas <douglas@douglas-laptop.(none)>
2012-10-09 11:34:38 -0300

    Estrutura do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formulate::Application.config.secret_token = '7dc33128b135aa7cd12f2f4c5648a503ee920f75abbcb51bfc433ffae3bed2b3bafa48c4d1ea9e427e2e76438c8cbed0aeb3267cfb5be997054f31422b1d78bb'
